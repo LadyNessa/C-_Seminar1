5f988f116e7acb0f629104b24e2e84512a32579b
Nessa <la-nessa@mail.ru>
2022-12-03 17:44:12 +0300

    Ex005

---
+++ Ex005/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите трехзначное число: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+if (100 < numberN)
+{
+    if (numberN < 1000)
+    {
+    double x = numberN % 10;
+    Console.WriteLine(x);
+    }
+    else
+    {
+    Console.WriteLine("Ошибка, введите трехзначное число: ");
+    }
+}
+
