59edca8744e262be3e47834dcbfe4d79efe38734
Nessa <la-nessa@mail.ru>
2022-12-03 15:44:11 +0300

    Ex003

---
+++ Ex003/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Введите номер дня недели: ");
+int numberD = Convert.ToInt32(Console.ReadLine());
+
+if (numberD == 1)
+{
+    Console.WriteLine("Понедельник");
+}
+if (numberD == 2)
+{
+    Console.WriteLine("Вторник");
+}
+if (numberD == 3)
+{
+    Console.WriteLine("Среда");
+}
+if (numberD == 4)
+{
+    Console.WriteLine("Четверг");
+}
+if (numberD == 5)
+{
+    Console.WriteLine("Пятница");
+}
+if (numberD == 6)
+{
+    Console.WriteLine("Суббота");
+}
+if (numberD == 7)
+{
+    Console.WriteLine("Воскресенье");
+}
