c73d9be376914b98dae7534e07f380ea88b5734e
Nessa <la-nessa@mail.ru>
2022-12-03 15:30:51 +0300

    Ex002

---
+++ Ex002/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+int quotient = number1 / number2;
+
+if (quotient == number2)
+{
+    Console.WriteLine("Да");
+}
+else 
+{
+    Console.WriteLine("Нет");
+}
