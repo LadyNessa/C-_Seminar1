49a7ba3bf11d75a5ec5aedc2b3ec89004c6deff6
Nessa <la-nessa@mail.ru>
2022-12-03 16:56:40 +0300

    Ex004

---
+++ Ex004/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+int x = -numberN;
+
+while (x <= numberN)
+{
+    Console.WriteLine(x);
+    x = x + 1;
+}
